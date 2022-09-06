990cff5dfec2fba11e126eb8c44eed3a74d5c7a7
Jim Schuh <jim.schuh@cgi.com>
2022-09-06 11:54:20 +0200

    deepdive initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.7-alpine
+
+RUN mkdir /app
+ADD . /app
+
+WORKDIR /app
+
+RUN pip3 install -r requirements.txt
+
+ENV FLASK_APP=app
+ENV FLASK_ENV=development
+
+CMD flask run --host=0.0.0.0 -p 5000
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
+@app.route('/')
+def start():
+    variable = "Greeting"
+    return render_template('start.html',
+        greeting = variable)
+
+@app.route('/question', methods=['GET', 'POST'])
+def question():
+    if request.method == 'POST':
+        yourName = request.form['your_name']
+        return render_template('answer.html',
+            name = yourName)
+    else:
+        return render_template('question.html')
+
+@app.route('/answer')
+def answer():
+    return render_template('answer.html',
+        name = yourName)
